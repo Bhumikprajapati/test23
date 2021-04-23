4c657770144f9fe6e42157e19bfc087a9f75a8f4
Bhumik.prajapati <bhumik.prajapati@bacancy.com>
2021-04-23 16:14:55 +0530

    task23

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import './likeButton.css';
 function App() {
+
+  const [count, setCount] = useState({
+    likeCount: 100,
+    dislikeCount: 25
+  })
+let likeCss=["like-button"]
+let dislikeCss=["dislike-button "]
+const likeCountHandler=()=>{
+  if(count.likeCount===101)
+  {
+    setCount({ ...count, likeCount: count.likeCount - 1 })
+    likeCss.pop("liked")
+  }
+  else{
+    setCount({ ...count, likeCount: count.likeCount + 1 })
+    likeCss.push("liked")
+  }
+}
+const dislikeCountHandler=()=>{
+  if(count.dislikeCount===26)
+  {
+    setCount({ ...count, dislikeCount: count.dislikeCount - 1 })
+    dislikeCss.pop("disliked")
+  }
+  else{
+    setCount({ ...count, dislikeCount: count.dislikeCount + 1 })
+    dislikeCss.push("disliked")
+  }
+} 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hii All</h1>
+      <h2> <button className={likeCss}
+       onClick={likeCountHandler}  >
+         Like|</button>
+        <span className="likes-counter" >{count.likeCount} </span>
+      </h2>
+      <h2> <button className={dislikeCss} 
+      onClick={dislikeCountHandler} >
+        Dislike|</button>
+        <span className="dislikes-counter"   >{count.dislikeCount} </span>
+      </h2>
     </div>
   );
 }
